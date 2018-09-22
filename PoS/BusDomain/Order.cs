8b432869f64ffab099ce1e173f063c089e7fbad9
jackshiels <SHLJAC006@myuct.ac.za>
2018-09-22 16:38:44 +0200

    Finalised Order methods

--- PoS/BusDomain/Order.cs
+++ PoS/BusDomain/Order.cs
@@ -25,8 +25,17 @@ namespace PoS.BusDomain
             Collection<OrderItem> itemList = new Collection<OrderItem>();
         }
 
+        public Order(Customer Cust)
+        {
+            // This constructor takes the address field from the customer
+            Collection<OrderItem> itemList = new Collection<OrderItem>();
+            this.owner = Cust;
+            this.address = Cust.Address;
+        }
+
         public Order(Customer Cust, string Address)
         {
+            // This constructor can take a custom address
             Collection<OrderItem> itemList = new Collection<OrderItem>();
             this.owner = Cust;
             this.address = Address;
@@ -34,6 +43,70 @@ namespace PoS.BusDomain
         #endregion
 
         #region Methods
+        public bool AddToOrder(Product prod, int quant)
+        {
+            // Adds a new product to the list if the list has been initialised
+            bool added = false;
+
+            if (itemList != null)
+            {
+                OrderItem item = new OrderItem(prod, quant);
+                itemList.Add(item);
+                added = true;
+            }
+
+            return added;
+        }
+
+        public bool RemoveFromOrder(int itemId)
+        {
+            // Checks if the item exists. Removes if it does.
+            int index = FindItem(itemId);
+            bool removed = false;
+
+            if (index != -1)
+            {
+                itemList.RemoveAt(index);
+                removed = true;
+            }
+
+            // Returns the effected var
+            return removed;
+        }
+
+        public bool UpdateOrderItemQuantity(int itemId, int quant)
+        {
+            // Finds the order item and updates the quantity
+            bool updated = false;
+            int index = FindItem(itemId);
+
+            if (index != -1)
+            {
+                itemList[index].Quantity = quant;
+                updated = true;
+            }
+
+            // returns the effected var
+            return updated;
+        }
+
+        public int FindItem(int itemId)
+        {
+            // Creates an index value. -1 indicates the item is not found
+            int index = -1;
+
+            for (int i = 0; i < itemList.Count(); i++)
+            {
+                if (itemList[i].OrderItemID == itemId)
+                {
+                    index = i;
+                }
+            }
+
+            // Returns the effected var
+            return index;
+        }
+
         public float CalculateTotal()
         {
             // This method should be called every time an OrderItem is added
